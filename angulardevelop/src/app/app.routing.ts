20f0168db51ba343f63a7436182052d235ec629c
damian knopp <dknopp@damians-MacBook-Pro.local>
2017-09-22 21:59:31 -0400

    fix lint

--- angulardevelop/src/app/app.routing.ts
+++ angulardevelop/src/app/app.routing.ts
@@ -4,33 +4,33 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { AppRouterPreloader } from './app-router-preloader';
 
 const routes: Routes = [
-    {
-      path: '',
-      redirectTo: 'home',
-      pathMatch: 'full'
-    },
-    {
-      path: 'home',
-      loadChildren: 'app/home/home.module#HomeModule',
-      data: { preload: true }
-    },
-    {
-      path: 'transitions',
-      loadChildren: 'app/transition-demo/transition-demo.module#TransitionDemoModule',
-    },
-    {
-      path: 'charts',
-      loadChildren: 'app/chart-demo/chart-demo.module#ChartDemoModule',
-    },
-    {
-      path: 'grid',
-      loadChildren: 'app/grid/grid.module#GridModule',
-    },
-    {
-      path: '**',
-      component: PageNotFoundComponent
-    }
-  ];
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: 'app/home/home.module#HomeModule',
+    data: { preload: true }
+  },
+  {
+    path: 'transitions',
+    loadChildren: 'app/transition-demo/transition-demo.module#TransitionDemoModule',
+  },
+  {
+    path: 'charts',
+    loadChildren: 'app/chart-demo/chart-demo.module#ChartDemoModule',
+  },
+  {
+    path: 'grid',
+    loadChildren: 'app/grid/grid.module#GridModule',
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+];
 
-export const routing: ModuleWithProviders = 
+export const routing: ModuleWithProviders =
   RouterModule.forRoot(routes, { enableTracing: false, preloadingStrategy: AppRouterPreloader });
